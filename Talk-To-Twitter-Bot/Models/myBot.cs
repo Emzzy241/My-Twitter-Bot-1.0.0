1b1d04d31d161c023758a0cf4a0ac3ea9058d28c
Emzzy241 <emzzyoluwole@gmail.com>
2023-03-09 20:22:40 +0100

    ading folder for all my C# Business Logic file

---
+++ Talk-To-Twitter-Bot/Models/myBot.cs
@@ -0,0 +1,6 @@
+// The business logic file of My application
+// I added the keyword public because I want my class to be accessed anywhere in my Application
+
+public class StartFromSecond{
+    
+}
